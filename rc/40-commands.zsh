92a074c70bbc374c3c9a049815e43d2e08d92f4b
Zachary Stigall <zirrostig@lanfort.org>
2014-10-28 13:18:05 -0600

    Added aliases for k commands

--- rc/40-commands.zsh
+++ rc/40-commands.zsh
@@ -25,6 +25,9 @@ alias la='ls -la'
 alias p=ls
 alias pl=ll
 alias pa=la
+alias ka='k -a'
+alias kd='k -d'
+alias kf='k -n'
 alias hexedit='vim +Vinarise'
 alias allcolors='(x=`tput op` y=`printf %80s`;for i in {0..255};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done)'
 alias grep='grep --color=auto'
